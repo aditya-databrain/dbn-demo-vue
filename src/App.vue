fd20ca6e34fe73d0f622e21591a1ef4a307ec664
Aditya Yaduvanshi <aditya@usedatabrain.com>
2023-06-14 09:06:00 +0530

    update plugin

--- src/App.vue
+++ src/App.vue
@@ -3,22 +3,17 @@ import 'dbn-ui-test-1/web'
 </script>
 
 <template>
+  <div class="dbn-dashboard">
     <dbn-dashboard
-      token="550975b6-5b5d-4a4b-9c2e-cfa9f260c4b6"
+      token="e0945419-7c0f-4390-9c0c-4c9ffcb4f735"
     />
+    </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.dbn-dashboard {
+  width: 100vw;
+  height: 100vh;
+  position: relative;
 }
 </style>
